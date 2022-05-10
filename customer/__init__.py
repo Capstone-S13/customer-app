69d20925ac0a0a378ae8234ebfc014ecca011439
rebbyling <rebecca.lingzw@gmail.com>
2022-05-10 14:44:46 +0200

    minor progress

--- customer/__init__.py
+++ customer/__init__.py
@@ -3,3 +3,17 @@ import os
 from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 import customer.views
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from customer import views
+
+    app.register_blueprint(views.login)
+
+    return app
--- customer/views.py
+++ customer/views.py
@@ -1,10 +1,20 @@
-from flask import render_template
+from flask import render_template, request, sesson, redirect, url_for
 from .__init__ import app
 
-@app.route('/')
-def index():
-    return 'hello'
 
-@app.route('/login')
+@app.route('/')
 def login():
-    return render_template('login.html')
+    error =  None
+    if request.method == 'POST':
+		x=request.form['username']
+		x_list=[Customer.query.all()[i].loginid for i in range(len(Customer.query.all()))]
+		if x in x_list:
+			z=Customer.query.filter_by(loginid=x).all()[0].passwd
+			if(request.form['password']!=z):
+				error = 'Invalid Credentials. Please try again.'
+			else:
+				session['username']=x #storing session variable
+				return render_template('index.html')
+    
+		error = 'Invalid Credentials. Please try again.' 
+    return render_template('login.html', error=error)
